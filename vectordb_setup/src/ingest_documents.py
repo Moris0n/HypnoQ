47eb3f87345e451a6d6b45c356af065ffd9c8ee9
Moris0n <146327569+Moris0n@users.noreply.github.com>
2024-08-29 15:02:04 +0000

    wip

--- vectordb_setup/src/ingest_documents.py
+++ vectordb_setup/src/ingest_documents.py
@@ -3,13 +3,14 @@ import dotenv
 from langchain.text_splitter import MarkdownHeaderTextSplitter
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.docstore.document import Document
 from langchain.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import TextLoader
 
 dotenv.load_dotenv(encoding='utf-8')
 
 # File paths and other configurations
-DATA_PATH = '../data'
+DATA_PATH = 'data'
 INDEX_NAME = 'hypnoq_index'
 
 # Initialize the document loader
@@ -29,11 +30,16 @@ headers_to_split_on = [
 # Initialize the MarkdownHeaderTextSplitter
 splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
 
-# Split the documents into smaller chunks
+# Split the documents into smaller chunks and combine headers with their respective text
 split_documents = []
 for document in documents:
     split_texts = splitter.split_text(document.page_content)
-    split_documents+=split_texts
+    for text_chunk in split_texts:
+        # Combine header with the text
+        header_content = " ".join([text_chunk["metadata"][label] for label in headers_to_split_on if label in text_chunk["metadata"]])
+        combined_text = f"{header_content}\n{text_chunk['content']}"
+        split_documents+=(Document(page_content=combined_text))
+
 
 # Initialize the embedding model (using OpenAI here, you can use others like HuggingFace)
 embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
